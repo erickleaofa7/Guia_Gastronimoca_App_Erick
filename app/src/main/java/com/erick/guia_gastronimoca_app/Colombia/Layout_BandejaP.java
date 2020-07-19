3de108a51e36dc45068ff4f04777220c5cbaac8c
Juan Cantuña <65374689+JuanKantu@users.noreply.github.com>
2020-07-18 22:31:38 -0500

    ActividadColombia

---
+++ app/src/main/java/com/erick/guia_gastronimoca_app/Colombia/Layout_BandejaP.java
@@ -0,0 +1,14 @@
+package com.erick.guia_gastronimoca_app.Colombia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Layout_BandejaP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout__bandeja_p);
+    }
+}
