92305f5929f40544ed52c0d1fe9368c46c49bfb1
jasson5p <jasson11@live.com>
2020-07-18 22:56:46 -0500

    ActividadVenezuela

---
+++ app/src/main/java/com/erick/guia_gastronimoca_app/Venezuela/Layout_Arepa.java
@@ -0,0 +1,14 @@
+package com.erick.guia_gastronimoca_app.Venezuela;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Layout_Arepa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout__arepa);
+    }
+}
